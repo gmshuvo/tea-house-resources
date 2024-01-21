bb0fd6283d577f78b7d97515bce68adb38d222d5
Golam Mostafa Shuvo <gmshuvo@deepmindlabs.ai>
2024-01-21 22:28:24 +0600

    ok slove

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['**/*.html', '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
   theme: {
     container: {
       center: true,
